3f6933bd1e4c61b4cebc51806ca486b5fff95903
E <1548-e@mail.net>
2019-07-26 00:32:35 -0500

    20190726

--- js/durapiso.index.js
+++ js/durapiso.index.js
@@ -9,202 +9,23 @@ let listClient = [];
 const sessionmaker = "LANDINGPAGE";
 // star LANDING PAGE
 OnStart();
+var myVar;
+
+function Test() {
+    myVar = setTimeout(alertFunc, 3000);
+}
+
+function alertFunc() {
+    console.dir(clients[1]);
+    alert("Hello!");
+    document.getElementById("client_description_0").innerHTML = clients[1].description;
+    document.getElementById("client_name_0").innerHTML = clients[1].name;   
+    $("#client_imgurl_0").attr("src", clients[1].imgurl);
+}
 
 // CLIENTS 
 function ClientsReadCallBack(clients) {
-
-    let tmphtml2 = `
-    <!-- Flexslider -->
-	<link rel="stylesheet" href="css/flexslider.css">
-    <div class="flexslider">
-
-							<ul class="slides">
-						 <li>
-									<blockquote>
-										<p>&ldquo;Descripcion del cliente .&rdquo;
-										</p>
-										<p><cite>&mdash; Cliente</cite></p>
-									</blockquote>
-								</li>
-								<li>
-									<blockquote>
-										<p>&ldquo;Client Review .&rdquo;
-										</p>
-										<p><cite>&mdash; Client</cite></p>
-									</blockquote>
-								</li> 
-                                </ul>
-                                </div>
-
-                                <script src="js/jquery.flexslider-min.js"></script>
-    `;
-    let tmphtml = `
-    
-    <style>
-    .carousel-inner > .item > img,
-    .carousel-inner > .item > a > img {
-      width: 25%;
-      margin: auto;
-    }
-    .carousel-control.right{
-        background-image: linear-gradient(to right, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.0001) 100%);
-    }
-
-    .carousel-control.left{
-        background-image: linear-gradient(to right, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.0001) 100%);
-    }
-    </style>
-    
-    <br>
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-        <li data-target="#myCarousel" data-slide-to="3"></li>
-      </ol>
-    
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-    
-        <div class="item active">
-          <img src="images/person1.png" alt="Chania" width="360" height="245">
-          <div class="carousel-caption">
-           
-          </div>
-        </div>
-    
-        <div class="item">
-          <img src="images/person2.png" alt="Chania" width="460" height="345">
-          <div class="carousel-caption">
-            
-          </div>
-        </div>
-      
-        <div class="item">
-          <img src="images/person3.png" alt="Flower" width="460" height="345">
-          <div class="carousel-caption">
-            
-          </div>
-        </div>
-    
-        <div class="item">
-          <img src="images/person1.png" alt="Flower" width="460" height="345">
-          <div class="carousel-caption">
-            
-          </div>
-        </div>
-    
-      </div>
-    
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>`; 
-
-    // $("#rowClientes").html(tmphtml);
-    // $("#rowClientes").html(tmphtml2);
-    let divClientsResenas = `
-    <div class="flexslider">
-    <ul class="slides">
-    <li>
-    <blockquote>
-        <p>&ldquo; Hola .&rdquo;
-        </p>
-        <p><cite>&mdash; Sepomex </cite></p>
-        <img src="./images/person3.png" width="25px" height="25px alt="Durapiso">
-    </blockquote>
-    </li>
-    <li>
-    <blockquote>
-        <p>&ldquo; Hola 2 .&rdquo;
-        </p>
-        <p><cite>&mdash; Imagen 2 </cite></p>
-        <img src="./images/person2.png" width="25px" height="25px alt="Durapiso">
-    </blockquote>
-    </li>
-    </ul>
-    </div>
-
-
-
-    `;
-
-
-
-    let divClients = `
-    <div class="flexslider-controls">
-    <ol class="flex-control-nav animate-box">
-    <li class="animate-box"><img src="images/person3.png" alt="Durapiso"></li>
-    <li class="animate-box"><img src="images/person3.png" alt="Durapiso"></li>
-    </ol>
-    
-    </div>
-    `;
-
-//     let myElement2 = document.getElementById("divClientsResenas");
-//     myElement2.innerHTML = divClientsResenas;
-
-//     let myElement = document.getElementById("divClients");
-//     myElement.innerHTML = divClients;
-
-//     // jQuery
-// $.getScript('./main.js', function()
-// {
-//     // script is now loaded and executed.
-//     // put your dependent JS here.
-//     testimonialFlexslider();
-
-// });
-
-    
-
-    // console.dir(products);
-
-    // let renderClients = '';
-    // let renderClientsResenas = '';
-
-    // clients.map(item => {
-    //     renderClients += `
-    //   <li class="animate-box"><img src="${item.imgurl}" alt="${item.description}"></li>   
-    //  `;
-
-    //  renderClientsResenas += `
-    //   <li>
-    //   <blockquote>
-    //   <p>&ldquo;${item.resenadurapiso} .&rdquo;
-    //   </p>
-    //   <p><cite>&mdash; ${item.name}</cite></p>
-    //   </blockquote>
-    //   </li>
-    // `;
-    // });
-    
-    // // let myElement = document.getElementById("divClients");
-    // renderClientsResenas = "<ul class='slides'>" + renderClients + "</ul>";
-    // let myElement2 = document.getElementById("divClientsResenas");
-    // // myElement.innerHTML = renderClients;
-    // myElement2.innerHTML = renderClientsResenas;
-
-    // var testimonialFlexslider = function() {
-    //     var $flexslider = $('.flexslider');
-    //     $flexslider.flexslider({
-    //       animation: "fade",
-    //       manualControls: ".flex-control-nav li",
-    //       directionNav: false,
-    //       smoothHeight: true,
-    //       useCSS: false /* Chrome fix*/
-    //     });
-    // };
-    // testimonialFlexslider();   
-
+    console.dir(clients);
 }
 
 function ClientsRead() {
@@ -263,7 +84,7 @@ function ProductsReadCallBack(products) {
         renderProducts += `
         <div class="col-md-4 col-sm-6 col-xxs-12 animate-box fadeInUp animated">
         <a href="${item.imgurl}" class="fh5co-project-item image-popup">
-            <img src="${item.imgurl}" alt="${item.description}" width="300px" height="300px" class="img-responsive">
+            <img src="${item.imgurl}" alt="${item.description}" class="img-responsive img-responsive-custom">
             <div class="fh5co-text">
                 <h2>${item.name}</h2>
                 <p>${item.description}</p>
